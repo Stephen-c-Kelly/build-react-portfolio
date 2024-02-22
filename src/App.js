33db35c80483616a6a7aa24a161d6964295d0300
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-02-22 16:49:24 -0500

    add navigation, index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import './App.css'
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Works from './components/Works';
+import Navigation from './components/Navigation';
+
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Home />
+      <About />
+      <Skills />
+      <Works />
+      <Contact />
       
     </div>
   );
