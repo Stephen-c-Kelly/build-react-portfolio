79900fa9d087d3a9659824e83375fc21e4da65f3
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-02-23 12:34:29 -0500

    add about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,46 @@
 import React from 'react'
 import { HiArrowNarrowRight   }  from 'react-icons/hi'
 import stephen from '../assets/stephen.png'
+import me from '../assets/me.png'
 import { Link } from 'react-scroll'
 
 const Home = () => {
-return (
-  <div
-    name='home'
-    className='h-screen w-full bg-[#0a192f]'
+  return (
+    <div
+    name="home"
+    className="h-screen w-full bg-[#0a192f]"
   >
-    <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-      <div className='flex flex-col justify-center h-full'>
-        <h2>Stephen Kelly is a Full Stack Developer, Client Services Expert, and Project Director</h2>
-      <p className='text-gray-500 py-4 max-w-md'>
-       With 10+ years working in creative technologies as both the client facing and project management lead, I’ve learned that aligning the goals of technical, creative, and client teams is the proven route to success. 
-      </p>
+    <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="flex flex-col justify-center h-full">
+        <h2 className="text-4xl sm:text-7xl font-bold text-white">
+          Building solutions and driving customer success
+        </h2>
+        <p className="text-gray-500 py-4 max-w-md">
+        Full-stack software engineer, customer success manager, and digital project director inspired by building products clients love. 
+        </p>
+        <div>
+          <Link
+            to="about"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+          >
+            About Me
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight size={25} className="ml-3" />
+            </span>
+          </Link>
+        </div>
       </div>
-
-
+      <div>
+        <img
+          src={stephen}  
+          alt="Image of Stephen Kelly"
+          className="rounded-2xl mx-auto w-2/3 md:w-full"
+        />
+      </div>
     </div>
-      
-      
   </div>
-  )
-}
-
+  );
+};
 export default Home
