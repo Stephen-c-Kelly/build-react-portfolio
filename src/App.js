2b1a907ae5435c250d9af38a627116054d226764
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-02-25 17:50:57 -0500

    add api call to homepage

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import Home from './components/Home';
 import Skills from './components/Skills';
 import Works from './components/Works';
 import Navigation from './components/Navigation';
-
+import {React, useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+const readtoken = process.env.BUTTER_CMS_READ_TOKEN
+const [data, setData] = useState([])
+
+useEffect(()=>{
+  const getData = async () =>{
+    axios.get(`https.//api.buttercms.com/v2/pages/portfolio/a-portfolio-site?auth_token=${readtoken}`)
+    .then(res =>{setData(res.data.data.fields.my_personal_porfolio)
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+  }
+  getData()
+}, [])
+
   return (
     <div >
       <Navigation />
-      <Home />
-      <About />
-      <Skills />
-      <Works />
-      <Contact />
+      <Home content={data[0]}/>
+      <About content={data[1]}/>
+      <Skills content={data[2]}/>
+      <Works content={data[3]}/>
+      <Contact/>
     </div>
   );
 }
