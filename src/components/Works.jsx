87f610fdccb1ad139bf99d51b83cf1adbb39a694
Stephen Kelly <stephen.c.kelly@gmail.com>
2024-03-04 17:54:54 -0500

    add styling update to work

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,73 +1,44 @@
 import React from 'react'
 
-const Works = ({content}) => {
-
-const projects = [
-  {
-    title: 'Whose in Charge?',
-    description: 'Whose in Charge? is an online quiz game built to help you memorize the names of world leaders. ',
-    demoUrl: 'https://world-leader-quiz.netlify.app',
-    codeUrl: 'https://github.com/Stephen-c-Kelly/WorldLeaderQuiz/blob/main/README.md',
-    backgroundImage: 'https://imgur.com/WpVLPEd'
-  },
-  {
-    title: 'PartySYNC',
-    description: 'Party SYNC lorem ipsum lorem ipsum lorem ipsum lorem ipsum. ',
-    demoUrl: 'https://world-leader-quiz.netlify.app',
-    codeUrl: 'https://github.com/Stephen-c-Kelly/WorldLeaderQuiz/blob/main/README.md',
-    backgroundImage: 'https://imgur.com/WpVLPEd'
-  }
-]
+const Works = ({ content }) => {
   return (
-  <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-    <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8 w-full flex justify-center items-center flex-col'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500 text-center'>
             Work
           </p>
-          <p className='py-6 text-2xl'>{content?.fields.work_header}</p>
+          <p className='py-6 text-2xl text-center'>{content?.fields.work_header}</p>
         </div>
-      {/* map of projects */}
-      {content?.fields.works.map((work,index)=>{
-        return(
-          <div style={{ backgroundImage: `url(${work.work_image})` }}
-          className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          key={index}
-          >
+        {/* map of projects */}
+        {content?.fields.works.map((work, index) => (
+          <div key={index} style={{ backgroundImage: `url(${work.work_image})` }} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div h-full mb-5">
             {/* hover effect */}
-          <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center mx-auto content-div'
-            >
-            <span className='text-lg font-bold text-white tracking-wider'>
-              {work.work_title}
-              </span>  
-            <p className='text-center'>
-              {work.work_description}
-              </p>  
-            <div className='pt-8 text-center'>
-              <a href={work.github_url}>
-                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'
-                >
-                  Demo
-                </button>
-              </a>
-              <a href={work.demo_url}>
-              <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                Code
-              </button>
-              </a>
+            <div className='opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center mx-auto p-4'>
+              <span className='text-lg font-bold text-white tracking-wider text-center'>
+                {work.work_title}
+              </span>
+              <p className='text-center'>
+                {work.work_description}
+              </p>
+              <div className='pt-8 text-center'>
+                <a href={work.github_url} target='_blank' rel="noopener noreferrer">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href={work.demo_url} target='_blank' rel="noopener noreferrer">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
             </div>
-          </div> 
-        </div> 
-        )
-      })}
-      {/* <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-        {projects.map((project, index)=>(
-          <WorkItem key={index} {...project} />
+          </div>
         ))}
-      </div> */}
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
--- src/components/sampleData.js
+++ src/components/sampleData.js
@@ -40,9 +40,9 @@ let sampleData=[
       work_header: 'Check out some of my most recent work',
       works: [
         {
-          work_title: 'Whose in Charge',
+          work_title: 'Who is in Charge',
           work_description: 
-            'Whose in Charge? is an online quiz game built to help you memorize the names of world leaders.',
+          'Host a party, invite friends, collab on tunes, all with PartySync!  Working with a team of developers, I project managed and built a full-stack app that uses the Python-based Django REST Framework with a React.js frontend',
           work_image: 'https://cdn.buttercms.com/CczKIP9USJmDQ44auXSA',
           github_url: 
             'https://github.com/Stephen-c-Kelly/WorldLeaderQuiz/blob/main/README.md',
@@ -50,6 +50,16 @@ let sampleData=[
         },
         {
           work_title: 'PartySYNC',
+          work_description: 
+            'Host a party, invite friends, collab on tunes, all with PartySync!\n' +
+            '\n' +
+            'Working with a team of developers, I project managed and built a full-stack app that uses the Python-based Django REST Framework with a React.js frontend',
+          work_image: 'https://cdn.buttercms.com/tzeqXaEMTGf3eKzzB3eg',
+          github_url: 'https://github.com/charlie-bedell/partysync-front-end/',
+          demo_url: 'https://partysync.netlify.app/'
+        },
+        {
+          work_title: 'ANOTHER ONE',
           work_description: 
             'Host a party, invite friends, collab on tunes, all with PartySync!\n' +
             '\n' +
